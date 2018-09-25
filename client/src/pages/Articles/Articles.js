36a106ef1d9bbe0da36a7c24ec9459b17d851821
Michael Schumann <schumann82@gmail.com>
2018-09-24 17:56:51 -0700

    starter files and folder structure

---
+++ client/src/pages/Articles/Articles.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import Jumbotron from "../../components/Jumbotron";
+import API from "../../utils/API";
+import DeleteBtn from "../../components/DeleteBtn";
+import { Col, Row, Container } from "../../components/Grid";
+import { List, ListItem } from "../../components/List";
+import { Input, TextArea, FormBtn } from "../../components/Form";
+
+class Articles extends Component {
+  state = {
+    articles: []
+  };
+
+  componentDidMount() {
+    this.loadArticles();
+  }
+
+  loadArticles = () => {
+    API.getArticles()
+      .then(res => this.setState({ articles: res.data }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col size="md-6">
+            <Jumbotron>
+              <h1>New York Times Article Scrubber</h1>
+              <h3>Search for and save articles of interest</h3>
+            </Jumbotron>
+            <form>
+              <Input name="topic" placeholder="Topic (required)" />
+              <Input name="start" placeholder="Start Year" />
+              <Input name="end" placeholder="End Year" />
+              <FormBtn>Search</FormBtn>
+            </form>
+          </Col>
+          <Col size="md-6 sm-12">
+            <Jumbotron>
+              <h1>Articles On My List</h1>
+            </Jumbotron>
+            {this.state.articles.length ? (
+              <List>
+                {this.state.articles.map(article => (
+                  <ListItem key={article._id}>
+                    <a href={"/articles/" + article._id}>
+                      <strong>
+                        {article.title}
+                        {article.url}
+                        {article.date}
+                      </strong>
+                    </a>
+                    <DeleteBtn />
+                  </ListItem>
+                ))}
+              </List>
+            ) : (
+              <h3>No Results to Display</h3>
+            )}
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Articles;
